aed405d77a06b55e3a3da777f1214f1ba5ed2320
Ismaellamgadar <ilamgadar@gmail.com>
2024-02-09 13:32:58 +0000

    Deploying to main from @ Ismaellamgadar/tictactoe@5a57de1e641dea4f46d4f81114bd2742a3d65d6f 🚀

--- README.md
+++
@@ -1,8 +0,0 @@
-## Tictactoe in Angular
-Lamgadar, Ismaël - 17.11.2023
-### TODOS:
-  - Text with players turn (Subscribable)
-  - Game logic (winner / loser)
-  - Game reset
-  - Light mode / dark mode
-
--- cypress/e2e/tictactoe.cy.ts
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:4200')
-  
-    cy.get('<table _ngcontent-qer-c12="" class="table"><tr _ngcontent-qer-c12="">');
-  })
-})
